f236f5f2953ded7efdeab7e90672fdfdd83c4ae7
matteoroxis <matteoroxis@gmail.com>
2026-01-12 14:40:22 +0100

    First commit of scaffolding project

---
+++ src/main/java/it/matteoroxis/grapholizer/GrapholizerApplication.java
@@ -0,0 +1,13 @@
+package it.matteoroxis.grapholizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapholizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapholizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/matteoroxis/grapholizer/GrapholizerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.matteoroxis.grapholizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapholizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
